edf64e12e4aac528556ee03992c78d7a3e1f821b
anil <amuseranil@gmail.com>
2024-09-14 07:05:50 -0500

    terraform

--- main.tf
+++ main.tf
@@ -1,22 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-  required_version = ">= 1.2.0"
-}
 
-provider "aws" {
-  region = "us-east-1"
-}
 
-resource "aws_instance" "mydemoserver" {
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.micro"
+# resource "aws_instance" "mydemoserver" {
+#   ami           = "ami-09c813fb71547fc4f"
+#   instance_type = "t3.micro"
 
-  tags = {
-    Name = "exampleinstance"
-  }
-}
+#   tags = {
+#     Name = "exampleinstance"
+#   }
+# }
