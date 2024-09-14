0fc77f5fa4f29213633a01b712a40c14e19a4c57
anil <amuseranil@gmail.com>
2024-09-14 07:34:04 -0500

    terraform

---
+++ variables/variables-file.tf
@@ -0,0 +1,37 @@
+variable "ami-id" {
+  type = string 
+  default = "ami-09c813fb71547fc4f"
+  description = "this is the AMI id for RHEL 9"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+  description = "this is AWS instance type"
+}
+
+variable "tags" {
+  type = map
+  default = {
+    Name ="backend"
+    Project = "expense"
+    Component = "backend"
+    Environment = "DEV"
+    Terraform = "true"
+  }
+}
+
+variable "sg_name" {
+  type = string
+  default = "allow-myssh"
+}
+
+variable "sg_description" {
+  type = string
+  default = "allow port 22 for ssh to allow all"
+}
+
+variable "ingres_cidr" {
+  type = list(string)
+  default = [ "0.0.0.0/0" ]
+}
