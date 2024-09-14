7757f50c362b42a7942e3d901f2beb0b2d77378c
anil <amuseranil@gmail.com>
2024-09-14 10:00:49 -0500

    terraform

--- conditions/outputs.tf
+++ conditions/outputs.tf
@@ -2,3 +2,8 @@ output "public_ip" {
   description = "this is the public IP of Ec2 instance"
   value = aws_instance.example.public_ip
 }
+
+output "private_ip" {
+  description = "this is the private IP of Ec2 instance"
+  value = aws_instance.example.private_ip
+}
