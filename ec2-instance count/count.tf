7989f68d1f33a9c5a5c2f7fce33c7f226bc691d1
anil <amuseranil@gmail.com>
2024-09-14 11:00:25 -0500

    terraform

---
+++ ec2-instance count/count.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "example" {
+    count = 3
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [ aws_security_group.allow-myssh.id ]
+    tags = {
+        Name = var.instance_names[count.index]
+    }
+}
+
+resource "aws_security_group" "allow-myssh" {
+  name = "allow-ssh"
+  description = "allow all ports through SSH"
+  egress {
+    from_port = 0
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol = "-1"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Allow-All-ssh"
+  }
+
+}
+
+
